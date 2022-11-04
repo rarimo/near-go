fc67f38f718bc898f417e9623f84b21e867b8cb8
Semen Loktionov <loktionov.kh@gmail.com>
2022-11-04 16:00:01 +0200

    fix tests

--- tests/balance_test.go
+++ tests/balance_test.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"gitlab.com/distributed_lab/kit/kv"
 	"gitlab.com/rarify-protocol/near-bridge-go/pkg/client/block"
+	"gitlab.com/rarify-protocol/near-bridge-go/pkg/types"
 	"gitlab.com/rarify-protocol/near-bridge-go/pkg/types/action"
 	"testing"
 )
@@ -14,7 +15,7 @@ import (
 func TestFtBalance(t *testing.T) {
 	cfg := NewConfig(context.Background(), kv.MustFromEnv())
 
-	res := map[string]string{"account_id": "napalmpapalam.testnet"}
+	res := map[string]string{"account_id": cfg.AccountID}
 	r, _ := json.Marshal(res)
 
 	x, _ := cfg.Client.ContractViewCallFunction(context.Background(), cfg.FtAddressOriginal, action.FtBalanceOfMethod, base64.StdEncoding.EncodeToString(r), block.FinalityFinal())
@@ -27,9 +28,11 @@ func TestFtBalance(t *testing.T) {
 func TestNftBalance(t *testing.T) {
 	cfg := NewConfig(context.Background(), kv.MustFromEnv())
 
-	res := map[string]string{"account_id": "napalmpapalam.testnet"}
+	res := map[string]string{"account_id": cfg.AccountID}
 	r, _ := json.Marshal(res)
 
 	x, _ := cfg.Client.ContractViewCallFunction(context.Background(), cfg.NftAddressOriginal, action.NftTokensForOwnerMethod, base64.StdEncoding.EncodeToString(r), block.FinalityFinal())
-	fmt.Println(x)
+	var z []types.NftView
+	json.Unmarshal(x.Result, &z)
+	fmt.Println(z)
 }
